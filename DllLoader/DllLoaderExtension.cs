c7a668dbf10cd7865be96db9617a23620a33bbd8
Rúben Garcia <rube200@users.noreply.github.com>
2020-10-18 00:01:34 +0100

    Remove SandBox related code

--- DllLoader/DllLoaderExtension.cs
+++ DllLoader/DllLoaderExtension.cs
@@ -22,11 +22,10 @@ namespace Oxide.Ext.DllLoader
 
         public override string Name => "DllLoader";
         public override string Author => "Rube200";
-        public override VersionNumber Version => new VersionNumber(1, 0, 2);
+        public override VersionNumber Version => new VersionNumber(1, 0, 3);
 
         public override void Load()
         {
-            RemoveSandBox();
             _pluginsLoader = new DllLoader();
             Manager.RegisterPluginLoader(_pluginsLoader);
             Interface.Oxide.OnFrame(OnFrame);
@@ -55,31 +54,5 @@ namespace Oxide.Ext.DllLoader
             foreach (var plugin in _pluginsLoader.LoadedPlugins.Values.OfType<CSharpPlugin>()
                 .Where(pl => pl.HookedOnFrame)) plugin.CallHook("OnFrame", args);
         }
-
-        #region SandBox
-
-        private readonly PropertyInfo _sandBoxPropertyInfo = typeof(CSharpExtension).GetProperty(
-            nameof(CSharpExtension.SandboxEnabled),
-            BindingFlags.Static | BindingFlags.Public);
-
-        private readonly bool _defaultSandBoxStat = CSharpExtension.SandboxEnabled;
-
-        private void RemoveSandBox()
-        {
-            if (_defaultSandBoxStat == false)
-                return;
-
-            _sandBoxPropertyInfo?.SetValue(null, false);
-        }
-
-        private void RestoreSandBox()
-        {
-            if (CSharpExtension.SandboxEnabled == _defaultSandBoxStat)
-                return;
-
-            _sandBoxPropertyInfo?.SetValue(null, _defaultSandBoxStat);
-        }
-
-        #endregion
     }
 }
