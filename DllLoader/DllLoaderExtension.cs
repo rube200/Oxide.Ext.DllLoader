ea53abce30a72e7aaa74e6111d003e843ce11b2c
Rúben Garcia <rube200@users.noreply.github.com>
2023-07-19 19:01:59 +0100

    Update DllLoaderExtension.cs

--- DllLoader/DllLoaderExtension.cs
+++ DllLoader/DllLoaderExtension.cs
@@ -1,34 +1,40 @@
 ﻿#region
 
-using System.Linq;
-using JetBrains.Annotations;
 using Oxide.Core;
 using Oxide.Core.Extensions;
-using Oxide.Plugins;
+using Oxide.Ext.DllLoader.Manager;
 
 #endregion
 
 namespace Oxide.Ext.DllLoader
 {
-    [UsedImplicitly]
+    // ReSharper disable once UnusedMember.Global
     public sealed class DllLoaderExtension : Extension
     {
-        private DllLoader _pluginsLoader;
-
-        public DllLoaderExtension(ExtensionManager manager) : base(manager)
-        {
-        }
-
         public override string Name => "DllLoader";
         public override string Author => "Rube200";
-        public override VersionNumber Version => new VersionNumber(1, 0, 4);
+        public override VersionNumber Version => new VersionNumber(1, 1, 1);
+
+        private DllLoaderPluginManager _pluginsLoader;
+
+
+        public DllLoaderExtension(ExtensionManager manager) : base(manager)
+        { }
+
 
         public override void Load()
         {
-            _pluginsLoader = new DllLoader();
+            _pluginsLoader = new DllLoaderPluginManager();
             Manager.RegisterPluginLoader(_pluginsLoader);
+
             Interface.Oxide.OnFrame(OnFrame);
         }
+
+        public override void LoadPluginWatchers(string pluginDirectory)
+        {
+ 
+        }
+
 
         public override void OnModLoad()
         {
@@ -40,18 +46,10 @@ namespace Oxide.Ext.DllLoader
             _pluginsLoader.OnShutdown();
         }
 
-        public override void LoadPluginWatchers(string pluginDirectory)
-        {
-            var dllWatcher = new DllLoaderWatcher(pluginDirectory);
-            _pluginsLoader.SetWatcher(dllWatcher);
-            Manager.RegisterPluginChangeWatcher(dllWatcher.Watcher);
-        }
-
         private void OnFrame(float delta)
         {
-            var args = new object[] {delta};
-            foreach (var plugin in _pluginsLoader.LoadedPlugins.Values.OfType<CSharpPlugin>()
-                .Where(pl => pl.HookedOnFrame)) plugin.CallHook("OnFrame", args);
+            foreach (var plugin in _pluginsLoader.OnFramePlugins)
+                plugin.CallHook("OnFrame", new [] { delta });
         }
     }
 }
